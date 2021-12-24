0c10dbd39a9bab7d43d29be75e31eef27f9a8398
Ram Aditya Nandula <nandulram@gmail.com>
2021-12-23 23:26:44 -0600

    Added scanning for reserved words and identifiers

--- com/craftinginterpreters/lox/Scanner.java
+++ com/craftinginterpreters/lox/Scanner.java
@@ -1,8 +1,9 @@
 package com.craftinginterpreters.lox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.zip.Adler32;
+import java.util.Map;
 
 import static com.craftinginterpreters.lox.TokenType.*;
 
@@ -12,8 +13,27 @@ class Scanner {
   private int start = 0; // first char in lexeme being scanned
   private int current = 0; // char currently being considered
   private int line = 1;
-  // var language = "lox" (20)
-  // *+-
+  private static final Map<String, TokenType> keywords;
+
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and", AND);
+    keywords.put("class", CLASS);
+    keywords.put("else", ELSE);
+    keywords.put("false", FALSE);
+    keywords.put("for", FOR);
+    keywords.put("fun", FUN);
+    keywords.put("if", IF);
+    keywords.put("nil", NIL);
+    keywords.put("or", OR);
+    keywords.put("print", PRINT);
+    keywords.put("return", RETURN);
+    keywords.put("super", SUPER);
+    keywords.put("this", THIS);
+    keywords.put("true", TRUE);
+    keywords.put("var", VAR);
+    keywords.put("while", WHILE);
+  }
 
   Scanner(String source) {
     this.source = source;
@@ -31,8 +51,7 @@ class Scanner {
   }
 
   private void scanToken() {
-    char c = advance(); // moves to the next char
-
+    char c = advance(); // moves to the next cha
     switch (c) {
       case '(':
         addToken(LEFT_PAREN);
@@ -100,11 +119,26 @@ class Scanner {
       default:
         if (isDigit(c)) {
           number();
+        } else if (isAlpha(c)) {
+          identifier();
         } else {
           Lox.error(line, "Unexpected character.");
         }
         break;
     }
+  }
+
+  // after scanning identifier, check if it is keyword
+  private void identifier() {
+    while (isAlphaNumeric(peek())) {
+      advance();
+    }
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+    if (type == null) {
+      type = IDENTIFIER;
+    }
+    addToken(type);
   }
 
   // consume as many digits for integer part
@@ -169,6 +203,14 @@ class Scanner {
       return '\0';
     }
     return source.charAt(current + 1);
+  }
+
+  private boolean isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') || (c >= 'A' || c <= 'Z') || c == '_';
+  }
+
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c);
   }
 
   private boolean isDigit(char c) {

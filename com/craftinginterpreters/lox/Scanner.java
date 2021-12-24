a6ff77a74ad7a946b0446cc885b59213a728c4ed
Ram Aditya Nandula <nandulram@gmail.com>
2021-12-23 23:49:07 -0600

    Fixed isAlpha error

--- com/craftinginterpreters/lox/Scanner.java
+++ com/craftinginterpreters/lox/Scanner.java
@@ -206,7 +206,7 @@ class Scanner {
   }
 
   private boolean isAlpha(char c) {
-    return (c >= 'a' && c <= 'z') || (c >= 'A' || c <= 'Z') || c == '_';
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
   }
 
   private boolean isAlphaNumeric(char c) {
